8221ab2fbae886a50b7e6defd73687769d629e84
shimizuhikaru <shimizuhikaru@gmail.com>
2023-09-29 11:43:29 +0900

    20230929

--- src/components/admin/adminLogin.tsx
+++ src/components/admin/adminLogin.tsx
@@ -126,62 +126,81 @@ const AdminLogin = () => {
 	};
 
 	return (
-		<Modal
-			// opened={openLoginWindow}
-			size="md"
-			opened={Boolean(authUser.uid === "")}
-			onClose={() => {
-				// setOpenLoginWindow(false);
-			}}
-			withCloseButton={false}
-			overlayProps={{
-				color: theme.colorScheme === "dark" ? theme.colors.dark[9] : theme.colors.gray[2],
-				opacity: 0.55,
-				blur: 20,
-			}}
-			padding="2em"
-		>
-			<Flex direction="column" align="center">
-				<PikaichiLogo2 width="8em" />
-				<Text mt="2em" fz="0.9em" lh="1.8em">
-					こちらはPikaichi inc.のwebサイトです。 モデル、タレント、およびその他のスタッフの権利を尊重するため、広く一般には公開しておりません。
-					弊社へ興味のある方やお仕事の依頼をご検討の方は、下記のアドレスまでメールをお送りください。 idおよびパスワードを発行いたします。
-				</Text>
-				<Anchor href="mailto:info@pikaichi-inc.com">info@pikaichi-inc.com</Anchor>
-				{errMsg && <ErrMsg msg={errMsg} />}
-			</Flex>
-
-			<Box component="form" onSubmit={handleSubmit} mt="3em" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-				<Box w="100%" sx={{ display: "flex", flexDirection: "column" }}>
-					{/* <FloatingLabelInput id="email" w="100%" mt="2em" size="md" withAsterisk={true} label="メールアドレス" placeholder="メールアドレス" form={form} /> */}
-
-					<FloatingLabelInputForPassWord id="pass" w="100%" mt="1em" size="md" label="パスワード" placeholder="パスワード" form={form} data-autofocus={true} />
-				</Box>
-
-				<Button
-					w="100%"
-					mt="2em"
-					onClick={(e) => {
-						handleSubmit(e);
-					}}
-				>
-					ログイン
-				</Button>
-				<Flex direction="column" align="center" mt="2em" fz="10pt">
-					<Box component="p">#1104, 2-14-6, TSUKIJI, CHUO-KU,</Box>
-					<Box component="p">TOKYO, 104-0045, JAPAN</Box>
-					<Anchor href="mailto:info@pikaichi-inc.com" color="none">
-						info@pikaichi-inc.com
-					</Anchor>
-					<Box component="p" mt="1em">
-						© PIKAICHI INC, ALL RIGHTS RESERVED.
-					</Box>
+		<>
+			<Modal
+				// opened={openLoginWindow}
+				size="md"
+				opened={Boolean(authUser.uid === "")}
+				onClose={() => {
+					// setOpenLoginWindow(false);
+				}}
+				withCloseButton={false}
+				overlayProps={{
+					color: "#FFF",
+					opacity: 0.4,
+					// blur: 20,
+				}}
+				padding="2em"
+			>
+				<Flex direction="column" align="center">
+					<PikaichiLogo2 width="8em" />
+					<Text mt="2em" fz="0.9em" lh="1.8em">
+						こちらはPikaichi inc.のwebサイトです。 モデル、タレント、およびその他のスタッフの権利を尊重するため、広く一般には公開しておりません。
+						弊社へ興味のある方やお仕事の依頼をご検討の方は、下記のアドレスまでメールをお送りください。 idおよびパスワードを発行いたします。
+					</Text>
+					<Anchor href="mailto:info@pikaichi-inc.com">info@pikaichi-inc.com</Anchor>
+					{errMsg && <ErrMsg msg={errMsg} />}
 				</Flex>
 
-				<LoadingOverlay visible={loading} />
-			</Box>
-		</Modal>
+				<Box component="form" onSubmit={handleSubmit} mt="3em" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+					<Box w="100%" sx={{ display: "flex", flexDirection: "column" }}>
+						{/* <FloatingLabelInput id="email" w="100%" mt="2em" size="md" withAsterisk={true} label="メールアドレス" placeholder="メールアドレス" form={form} /> */}
 
+						<FloatingLabelInputForPassWord
+							id="pass"
+							w="100%"
+							mt="1em"
+							size="md"
+							label="パスワード"
+							placeholder="パスワード"
+							form={form}
+							data-autofocus={true}
+						/>
+					</Box>
+
+					<Button
+						w="100%"
+						mt="2em"
+						onClick={(e) => {
+							handleSubmit(e);
+						}}
+					>
+						ログイン
+					</Button>
+					<Flex direction="column" align="center" mt="2em" fz="10pt">
+						<Box component="p">#1104, 2-14-6, TSUKIJI, CHUO-KU,</Box>
+						<Box component="p">TOKYO, 104-0045, JAPAN</Box>
+						<Anchor href="mailto:info@pikaichi-inc.com" color="none">
+							info@pikaichi-inc.com
+						</Anchor>
+						<Box component="p" mt="1em">
+							© PIKAICHI INC, ALL RIGHTS RESERVED.
+						</Box>
+					</Flex>
+
+					<LoadingOverlay visible={loading} />
+				</Box>
+			</Modal>
+			<Box
+				component="video"
+				src="/movie/sunlight.mp4"
+				autoPlay
+				loop
+				muted
+				playsInline
+				sx={{ position: "fixed", top: 0, right: 0, left: 0, bottom: 0, overflow: "hidden", minWidth: "100%", minHeight: "100%" }}
+			/>
+		</>
 		// 		<div>
 		// 			<AdminHeader title={"ログイン"} />
 		//
