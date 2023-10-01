34fb0c5ffc1de6b56418234c47dd717a93642e31
shimizuhikaru <shimizuhikaru@gmail.com>
2023-10-01 15:10:36 +0900

    20231001

---
+++ src/pages/works/donbee.tsx
@@ -0,0 +1,57 @@
+import path from "path";
+import sizeOf from "image-size";
+
+import { Box, Flex, Title } from "@mantine/core";
+import NextImage from "next/future/image";
+
+import { WorksDataType, worksData } from "../../data/worksData";
+import { WorksWrapperWithCollapse } from "../../components/worksComponent/worksWrapperWithCollapse";
+
+const workId = "donbee";
+export default function Donbee({ data }: { data: WorksDataType }) {
+	//
+
+	return (
+		<WorksWrapperWithCollapse data={data} title={data.titleEn}>
+			<Flex direction="column" align="center" mt="2em" gap="8em">
+				<Box
+					component={NextImage}
+					src="/img/works/donbee/donbee_03.jpg"
+					alt="Picture of the author"
+					w="75%"
+					h="fit-content"
+					width={1772}
+					height={918}
+					sx={{ objectFit: "contain" }}
+				/>
+			</Flex>
+		</WorksWrapperWithCollapse>
+	);
+}
+
+export async function getStaticProps() {
+	//
+	const imgPath = ["public", "img", "works"];
+	const imgDirectory = path.join(process.cwd(), ...imgPath);
+
+	const nWorksData = worksData.map((d) => {
+		const fullPathPC = path.join(imgDirectory, d.filePathPC);
+		const fullPathSP = path.join(imgDirectory, d.filePathSP);
+		const dimensionsPC = sizeOf(fullPathPC);
+		const dimensionsSP = sizeOf(fullPathSP);
+		return {
+			...d,
+			srcPC: `/${imgPath[1]}/${imgPath[2]}/${d.filePathPC}`,
+			srcSP: `/${imgPath[1]}/${imgPath[2]}/${d.filePathSP}`,
+			widthPC: dimensionsPC.width,
+			widthSP: dimensionsSP.width,
+			heightPC: dimensionsPC.height,
+			heightSP: dimensionsSP.height,
+		};
+	});
+
+	const data = nWorksData.find((d) => d.id === workId);
+	return {
+		props: { data: data },
+	};
+}
